8444a4c01ef9fae4698f741d2a119bcd982cece4
root <root@kali.brax>
2019-02-20 01:49:57 -0500

    full url to output

--- libgrequest/fuzzrequest.go
+++ libgrequest/fuzzrequest.go
@@ -45,76 +45,127 @@ func (rh *RedirectHandler) RoundTrip(req *http.Request) (resp *http.Response, er
 	return resp, err
 }
 
-func getFUZZreq(s *State, u string) (*http.Request, error) {
-	if s.Post {
-		if s.PostForm {
-			// TODO: config payload string
-			v := url.Values{}
-			pairs := strings.Split(s.Payload, ",")
-			for i := range pairs {
-				kv := strings.Split(pairs[i], "=")
-				if len(kv) == 2 {
-					v.Set(kv[0], kv[1])
-				}
-			}
-			encv := v.Encode()
-			req, err := http.NewRequest("POST", u, strings.NewReader(encv))
-			if err != nil {
-				return nil, nil
-			}
-			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-			return req, err
-		}
-		if s.PostMulti {
-			var err error
-			values := map[string]io.Reader{
-				// "file":  mustOpen("main.go"), // lets assume its this file
-				"other": strings.NewReader(s.Payload),
-			}
-			var b bytes.Buffer
-			multipartw := multipart.NewWriter(&b)
-			for key, rdr := range values {
-				var fwtr io.Writer
-				if f, ok := rdr.(io.Closer); ok {
-					defer f.Close()
-				}
-				if f, ok := rdr.(*os.File); ok {
-					if fwtr, _ = multipartw.CreateFormFile(key, f.Name()); err != nil {
-						return nil, nil
-					}
-				} else {
-					if fwtr, err = multipartw.CreateFormField(key); err != nil {
-						return nil, nil
-					}
-				}
-				if _, err = io.Copy(fwtr, rdr); err != nil {
-					return nil, err
-				}
-			}
-			multipartw.Close()
-			req, err := http.NewRequest("POST", u, &b)
-			if err != nil {
-				return nil, nil
-			}
-			req.Header.Add("Content-Type", multipartw.FormDataContentType())
-			return req, err
+func makePostFormRequest(s *State, fullURL, cookie string) (*int, error) {
+	s.Counter.Inc()
+	v := url.Values{}
+	pairs := strings.Split(s.Payload, ",")
+	for i := range pairs {
+		kv := strings.Split(pairs[i], "=")
+		if len(kv) == 2 {
+			v.Set(kv[0], kv[1])
 		}
 	}
-	// If GET request
-	req, err := http.NewRequest("GET", u, nil)
+	encv := v.Encode()
+	req, err := http.NewRequest("POST", fullURL, strings.NewReader(encv))
 	if err != nil {
 		return nil, nil
 	}
-	return req, err
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if cookie != "" {
+		req.Header.Set("Cookie", cookie)
+	}
+	if s.UserAgent != "" {
+		req.Header.Set("User-Agent", s.UserAgent)
+	}
+	if s.Username != "" {
+		req.SetBasicAuth(s.Username, s.Password)
+	}
+	resp, err := s.Client.Do(req)
+	if err != nil {
+		if ue, ok := err.(*url.Error); ok {
+			if strings.HasPrefix(ue.Err.Error(), "x509") {
+				fmt.Println("[!] Invalid certificate, try using -k.")
+			}
+			if re, ok := ue.Err.(*RedirectError); ok {
+				return &re.StatusCode, nil
+			}
+		}
+		return nil, nil
+	}
+	defer resp.Body.Close()
+
+	r, err := InitResult(fullURL, resp)
+	if err != nil {
+		return nil, nil
+	}
+	// Print Output
+	if s.Quiet != true {
+		s.Printer(s, r)
+	}
+	return &resp.StatusCode, nil
+}
+
+func makePostMultiRequest(s *State, fullURL, cookie string) (*int, error) {
+	s.Counter.Inc()
+	var err error
+	values := map[string]io.Reader{
+		// "file":  mustOpen("main.go"), // lets assume its this file
+		"other": strings.NewReader(s.Payload),
+	}
+	var b bytes.Buffer
+	multipartw := multipart.NewWriter(&b)
+	for key, rdr := range values {
+		var fwtr io.Writer
+		if f, ok := rdr.(io.Closer); ok {
+			defer f.Close()
+		}
+		if f, ok := rdr.(*os.File); ok {
+			if fwtr, _ = multipartw.CreateFormFile(key, f.Name()); err != nil {
+				return nil, nil
+			}
+		} else {
+			if fwtr, err = multipartw.CreateFormField(key); err != nil {
+				return nil, nil
+			}
+		}
+		if _, err = io.Copy(fwtr, rdr); err != nil {
+			return nil, err
+		}
+	}
+	multipartw.Close()
+	req, err := http.NewRequest("POST", fullURL, &b)
+	if err != nil {
+		return nil, nil
+	}
+	req.Header.Add("Content-Type", multipartw.FormDataContentType())
+	if cookie != "" {
+		req.Header.Set("Cookie", cookie)
+	}
+	if s.UserAgent != "" {
+		req.Header.Set("User-Agent", s.UserAgent)
+	}
+	if s.Username != "" {
+		req.SetBasicAuth(s.Username, s.Password)
+	}
+	resp, err := s.Client.Do(req)
+	if err != nil {
+		if ue, ok := err.(*url.Error); ok {
+			if strings.HasPrefix(ue.Err.Error(), "x509") {
+				fmt.Println("[!] Invalid certificate, try using -k.")
+			}
+			if re, ok := ue.Err.(*RedirectError); ok {
+				return &re.StatusCode, nil
+			}
+		}
+		return nil, nil
+	}
+	defer resp.Body.Close()
+
+	r, err := InitResult(fullURL, resp)
+	if err != nil {
+		return nil, nil
+	}
+	// Print Output
+	if s.Quiet != true {
+		s.Printer(s, r)
+	}
+	return &resp.StatusCode, nil
 }
 
 // makeRequest : make http request
 func makeRequest(s *State, fullURL, cookie string) (*int, error) {
 	s.Counter.Inc()
 	req, err := http.NewRequest("GET", fullURL, nil)
-	if err != nil {
-		return nil, nil
-	}
 	if err != nil {
 		return nil, nil
 	}
@@ -155,4 +206,14 @@ func makeRequest(s *State, fullURL, cookie string) (*int, error) {
 // GoGet : returs address of response statuscode and error
 func GoGet(s *State, url, cookie string) (*int, error) {
 	return makeRequest(s, url, cookie)
+}
+
+// GoPostForm :
+func GoPostForm(s *State, url, cookie string) (*int, error) {
+	return makePostFormRequest(s, url, cookie)
+}
+
+// GoPostMultiPart :
+func GoPostMultiPart(s *State, url, cookie string) (*int, error) {
+	return makePostMultiRequest(s, url, cookie)
 }
